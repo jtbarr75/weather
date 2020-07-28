cfa03c9f1308df4b139ea4f7c029f10556f877e8
Jason Barr <Barr75@gmail.com>
2020-07-27 17:41:31 -0700

    background and icon changes to match weather

--- index.js
+++ index.js
@@ -9,6 +9,84 @@ const weather = (function () {
     }
   })
 
+  function kToF(degrees) {
+    return Math.floor((degrees - 273.15) * 9/5 + 32);
+  }
+
+  function getTime(offset) {
+    const d = new Date();
+    const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    return new Date(utc + (offset*1000));
+  }
+
+  function update(data) {
+    const location = document.getElementById("location");
+    const description = document.getElementById("description");
+    const temp = document.getElementById("temp");
+    if (data.weather) {
+      const weather = data.weather[0].main;
+      const desc = data.weather[0].description;
+      const time = getTime(data.timezone).getHours();
+    
+      location.textContent = data.name;
+      description.textContent = desc[0].toUpperCase() + desc.slice(1);
+      temp.textContent = kToF(data.main.temp) +"\xB0";
+  
+      setIcon(weather, time);
+    } else {
+      location.textContent = "Not Found";
+      description.textContent = "";
+      temp.textContent = ""
+    }
+  }
+
+  function setIcon(weather, time) {
+    if (weather === "Clear") {
+      (time > 6 && time < 19) ? setWeather("clear", "sun") : setWeather("night", "moon");
+    } else if (weather === "Clouds" || weather === "Mist" || weather === "Haze") {
+      (time > 6 && time < 19) ? setWeather("cloudy-light", "cloud") : setWeather("cloudy-dark", "cloud");
+    } else if (weather === "Rain" || weather === "Drizzle") {
+      (time > 6 && time < 19) ? setWeather("cloudy-light", "cloud", "rain") : setWeather("cloudy-dark", "cloud", "rain");
+    } else if (weather === "Thunderstorm" || weather === "Squall") {
+      setWeather("cloudy-dark", "cloud", "rain");
+    } else if (weather === "Snow") {
+      (time > 6 && time < 19) ? setWeather("cloudy-light", "cloud", "snow") : setWeather("cloudy-dark", "cloud", "snow");
+    } else if (weather === "Dust" || weather === "Sand") {
+      setWeather("sandy", "cloud");
+    } else if (weather === "Ash" || weather === "Tornado") {
+      setWeather("cloudy-dark", "cloud")
+    }
+  }
+
+  function setWeather(type, icon, more = "") {
+    const background = document.querySelector(".background");
+    background.className = "background center " + type;
+
+    const iconWrapper = document.querySelector(".icon-wrapper");
+    const weather = document.createElement("div");
+    weather.className = icon;
+    if (iconWrapper.firstChild) {
+      iconWrapper.removeChild(iconWrapper.firstChild);
+    }
+    iconWrapper.appendChild(weather);
+
+    if (more) {
+      const cloud = document.querySelector(".cloud");
+      const drops = document.createElement("div");
+      drops.className = more;
+      addDrops(drops);
+      cloud.appendChild(drops);
+    }
+  }
+
+  function addDrops(parent) {
+    for (let i=1; i<16; i++) {
+      const drop = document.createElement("div");
+      drop.className = `drop d${i}`;
+      parent.appendChild(drop);
+    }
+  }
+
   async function search() {
     if (input.value) {
       const search = `${url}${input.value}&appid=${API_KEY}`;
@@ -16,6 +94,7 @@ const weather = (function () {
         let response = await fetch(search, {mode: 'cors'});
         let data = await response.json();
         console.log(data);
+        update(data);
       } catch (err) {
         console.log(err);
       }
